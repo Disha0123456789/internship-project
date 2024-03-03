23f8d33c349f25f64567d279818ab1ca59a30444
Ajay01103 <125455606+Ajay01103@users.noreply.github.com>
2024-03-03 16:04:45 +0530

    Ajay's commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,28 @@
 // import { Container } from "react-bootstrap";
 // import FutureBoxes from "./assets/Boxes/FutureBoxes";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 // import Header from "./assets/Boxes/Header";
-import TarotReading from "./Page/TarotReading/TarotReading";
-import Meditation from "./components/meditation/Meditation";
-import DailyMeditation from "./components/meditation/DailyMeditation";
+import TarotReading from "./Page/TarotReading/TarotReading"
+import Meditation from "./components/meditation/Meditation"
+import DailyMeditation from "./components/meditation/DailyMeditation"
 
-import Home from "./Page/Home/Home";
-import Facecard from "./assets/Boxes/Facecard";
+import Home from "./Page/Home/Home"
+import Facecard from "./assets/Boxes/Facecard"
 
-import Faceresult from "./assets/Boxes/Facecard/Faceresult";
-import Timemagicresult from "./assets/Boxes/Facecard/Timemagicresult";
-import Ancestors from "./assets/Boxes/Facecard/Ancestors";
-import Palmreader from "./assets/Boxes/Facecard/Palmreader";
-import FingerReading from "./assets/Boxes/Facecard/FingerReading";
+import Faceresult from "./assets/Boxes/Facecard/Faceresult"
+import Timemagicresult from "./assets/Boxes/Facecard/Timemagicresult"
+import Ancestors from "./assets/Boxes/Facecard/Ancestors"
+import Palmreader from "./assets/Boxes/Facecard/Palmreader"
 
-import ChakraMeditation from "./components/meditation/ChakraMeditation";
-import SleepMeditation from "./components/meditation/SleepMeditation";
-import FiveMinMeditation from "./components/meditation/FiveMinMeditation";
-import Mindfulness from "./components/meditation/Mindfulness";
-import PositiveMeditation from "./components/meditation/PositiveMeditation";
-import BeginnerMeditation from "./components/meditation/BeginnerMeditation";
-import MentalHealthMeditation from "./components/meditation/MentalHealthMeditation";
-import EpicPowerMeditation from "./components/meditation/EpicPowerMeditation";
+import ChakraMeditation from "./components/meditation/ChakraMeditation"
+import SleepMeditation from "./components/meditation/SleepMeditation"
+import FiveMinMeditation from "./components/meditation/FiveMinMeditation"
+import Mindfulness from "./components/meditation/Mindfulness"
+import PositiveMeditation from "./components/meditation/PositiveMeditation"
+import BeginnerMeditation from "./components/meditation/BeginnerMeditation"
+import MentalHealthMeditation from "./components/meditation/MentalHealthMeditation"
+import EpicPowerMeditation from "./components/meditation/EpicPowerMeditation"
+import { FortuneWheel } from "./components/wheel/FortuneWheel"
 
 function App() {
   return (
@@ -33,12 +33,13 @@ function App() {
         <Route path="/Faceresult" element={<Faceresult />} />
         <Route path="/Meditation" element={<Meditation />} />
 
+        <Route path="/Fortune_Wheel" element={<FortuneWheel />} />
+
         <Route path="/Facecard" element={<Facecard />} />
         <Route path="/Timemagicresult" element={<Timemagicresult />} />
         <Route path="/Ancestors" element={<Ancestors />} />
 
         <Route path="/Palmreader" element={<Palmreader />} />
-        <Route path="/FingerReading" elemnt={<FingerReading />} />
 
         <Route path="/DailyMeditation" element={<DailyMeditation />} />
         <Route path="/ChakraMeditation" element={<ChakraMeditation />} />
@@ -57,10 +58,9 @@ function App() {
         <Route path="/EpicPowerMeditation" element={<EpicPowerMeditation />} />
 
         <Route path="/Facecard" element={<Facecard />} />
-
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
