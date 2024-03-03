dc4f25572ee467baecb72f96bf396dc5da7ed8a7
unknown <jaybhuptani1054@gmail.com>
2024-03-03 21:25:09 +0530

    maintain

--- src/components/wheel/FortuneWheel.jsx
+++ src/components/wheel/FortuneWheel.jsx
@@ -1,4 +1,4 @@
-import WheelComponent from "react-wheel-of-prizes"
+import WheelComponent from "react-wheel-of-prizes";
 // import "react-wheel-of-prizes/dist/index.css"
 // import "react-wheel-of-prizes/dist/index.css"
 
@@ -12,7 +12,7 @@ export const FortuneWheel = () => {
     "won uber pass",
     "better luck next time",
     "won a voucher",
-  ]
+  ];
   const segColors = [
     "#EE4040",
     "#F0CF50",
@@ -22,28 +22,29 @@ export const FortuneWheel = () => {
     "#F9AA1F",
     "#EC3F3F",
     "#FF9000",
-  ]
-  const onFinished = winner => {
-    console.log(winner)
-  }
+  ];
+  const onFinished = (winner) => {
+    console.log(winner);
+  };
   return (
-    <div className="relative flex flex-col items-center justify-center w-full h-full">
-     <div className="">
-      <WheelComponent
-        segments={segments}
-        segColors={segColors}
-        winningSegment="won 10 || won 70"
-        onFinished={winner => onFinished(winner)}
-        primaryColor="black"
-        contrastColor="white"
-        buttonText="Spin"
-        isOnlyOnce={false}
-        size={290}
-        upDuration={1000}
-        downDuration={1000}
-        fontFamily="Arial"
-      />
-     </div>
-    </div>
-  )
-}
+    <h1>hy</h1>
+    // <div className="relative flex flex-col items-center justify-center w-full h-full">
+    //  <div className="">
+    //   <WheelComponent
+    //     segments={segments}
+    //     segColors={segColors}
+    //     winningSegment="won 10 || won 70"
+    //     onFinished={winner => onFinished(winner)}
+    //     primaryColor="black"
+    //     contrastColor="white"
+    //     buttonText="Spin"
+    //     isOnlyOnce={false}
+    //     size={290}
+    //     upDuration={1000}
+    //     downDuration={1000}
+    //     fontFamily="Arial"
+    //   />
+    //  </div>
+    // </div>
+  );
+};
