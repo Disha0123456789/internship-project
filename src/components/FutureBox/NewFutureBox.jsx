5c796344014d4f94aacb1ec8651c3c1728135a03
Ajay01103 <125455606+Ajay01103@users.noreply.github.com>
2024-03-04 21:19:23 +0530

    Ajay's commit

--- src/components/FutureBox/NewFutureBox.jsx
+++ src/components/FutureBox/NewFutureBox.jsx
@@ -16,7 +16,7 @@ const NewFutureBox = () => {
             </div>
 
             <div class="mt-3">
-              <h3 class="text-sm sm:text-lg font-semibold text-gray-800 dark:text-gray-200">
+              <h3 class="text-md sm:text-lg font-semibold text-gray-800 dark:text-gray-200">
                 Tarrot Reading
               </h3>
             </div>
@@ -31,7 +31,7 @@ const NewFutureBox = () => {
             </div>
 
             <div class="mt-3">
-              <h3 class="text-sm sm:text-lg font-semibold text-gray-800 dark:text-gray-200">
+              <h3 class="text-md sm:text-lg font-semibold text-gray-800 dark:text-gray-200">
                 Angel Reading
               </h3>
             </div>
@@ -46,7 +46,7 @@ const NewFutureBox = () => {
             </div>
 
             <div class="mt-3">
-              <h3 class="text-sm sm:text-lg font-semibold text-gray-800 dark:text-gray-200">
+              <h3 class="text-md sm:text-lg font-semibold text-gray-800 dark:text-gray-200">
                 Ketina Love
               </h3>
             </div>
@@ -61,7 +61,7 @@ const NewFutureBox = () => {
             </div>
 
             <div class="mt-3">
-              <h3 class="text-sm sm:text-lg font-semibold text-gray-800 dark:text-gray-200">
+              <h3 class="text-md sm:text-lg font-semibold text-gray-800 dark:text-gray-200">
                 Face Reader
               </h3>
             </div>
@@ -76,7 +76,7 @@ const NewFutureBox = () => {
             </div>
 
             <div class="mt-3">
-              <h3 class="text-sm sm:text-lg font-semibold text-gray-800 dark:text-gray-200">
+              <h3 class="text-md sm:text-lg font-semibold text-gray-800 dark:text-gray-200">
                 Palm Reader
               </h3>
             </div>
@@ -91,7 +91,7 @@ const NewFutureBox = () => {
             </div>
 
             <div class="mt-3">
-              <h3 class="text-sm sm:text-lg font-semibold text-gray-800 dark:text-gray-200">
+              <h3 class="text-md sm:text-lg font-semibold text-gray-800 dark:text-gray-200">
                 Finger Reader
               </h3>
             </div>
@@ -112,7 +112,7 @@ const NewFutureBox = () => {
             </div>
 
             <div class="mt-3">
-              <h3 class="text-sm sm:text-lg font-semibold text-gray-800 dark:text-gray-200">
+              <h3 class="text-md sm:text-lg font-semibold text-gray-800 dark:text-gray-200">
                 Time Travel
               </h3>
             </div>
@@ -127,7 +127,7 @@ const NewFutureBox = () => {
             </div>
 
             <div class="mt-3">
-              <h3 class="text-sm sm:text-lg font-semibold text-gray-800 dark:text-gray-200">
+              <h3 class="text-md sm:text-lg font-semibold text-gray-800 dark:text-gray-200">
                 Astrology
               </h3>
             </div>
@@ -142,7 +142,7 @@ const NewFutureBox = () => {
             </div>
 
             <div class="mt-3">
-              <h3 class="text-sm sm:text-lg font-semibold text-gray-800 dark:text-gray-200">
+              <h3 class="text-md sm:text-lg font-semibold text-gray-800 dark:text-gray-200">
                 Biorhythm
               </h3>
             </div>
@@ -152,7 +152,7 @@ const NewFutureBox = () => {
       </div>
 
       <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
-        <h1 className="text-3xl pb-6 font-semibold">Lets Take You To Another Dimension</h1>
+        <h1 className="text-3xl pb-6 font-semibold">Lets go To Another Dimension</h1>
 
         <div class="grid grid-cols-1 lg:grid-cols-3 gap-10 md:gap-14">
           <div class="h-36 sm:h-56 flex flex-col justify-center border bg-gradient-to-tr from-purple-300 to-blue-300 rounded-xl text-center p-4 md:p-5 dark:border-gray-700">
@@ -164,7 +164,7 @@ const NewFutureBox = () => {
             </div>
 
             <div class="mt-3">
-              <h3 class="text-sm sm:text-lg font-semibold text-gray-800 dark:text-gray-200">
+              <h3 class="text-md sm:text-lg font-semibold text-gray-800 dark:text-gray-200">
                 Dream Interpretation
               </h3>
             </div>
@@ -179,7 +179,7 @@ const NewFutureBox = () => {
             </div>
 
             <div class="mt-3">
-              <h3 class="text-sm sm:text-lg font-semibold text-gray-800 dark:text-gray-200">
+              <h3 class="text-md sm:text-lg font-semibold text-gray-800 dark:text-gray-200">
                 Crystalball
               </h3>
             </div>
@@ -194,7 +194,7 @@ const NewFutureBox = () => {
             </div>
 
             <div class="mt-3">
-              <h3 class="text-sm sm:text-lg font-semibold text-gray-800 dark:text-gray-200">
+              <h3 class="text-md sm:text-lg font-semibold text-gray-800 dark:text-gray-200">
                 Numerology
               </h3>
             </div>
@@ -204,7 +204,7 @@ const NewFutureBox = () => {
       </div>
 
       <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
-        <h1 className="text-3xl pb-6 font-semibold">Embark On Your Journey</h1>
+        <h1 className="text-3xl pb-6 font-semibold">Nourish Your Soul</h1>
 
         <div class="grid grid-cols-1 lg:grid-cols-3 gap-10 md:gap-14">
           <div class="h-36 sm:h-56 flex flex-col justify-center border bg-gradient-to-tr from-purple-300 to-blue-300 rounded-xl text-center p-4 md:p-5 dark:border-gray-700">
@@ -216,7 +216,7 @@ const NewFutureBox = () => {
             </div>
 
             <div class="mt-3">
-              <h3 class="text-sm sm:text-lg font-semibold text-gray-800 dark:text-gray-200">
+              <h3 class="text-md sm:text-lg font-semibold text-gray-800 dark:text-gray-200">
                 Meditation
               </h3>
             </div>
@@ -231,7 +231,7 @@ const NewFutureBox = () => {
             </div>
 
             <div class="mt-3">
-              <h3 class="text-sm sm:text-lg font-semibold text-gray-800 dark:text-gray-200">
+              <h3 class="text-md sm:text-lg font-semibold text-gray-800 dark:text-gray-200">
                 Emotional Support
               </h3>
             </div>
@@ -246,7 +246,7 @@ const NewFutureBox = () => {
             </div>
 
             <div class="mt-3">
-              <h3 class="text-sm sm:text-lg font-semibold text-gray-800 dark:text-gray-200">
+              <h3 class="text-md sm:text-lg font-semibold text-gray-800 dark:text-gray-200">
                 Get Your Solution
               </h3>
             </div>
