792e73827c654b2a926113687472f4ea57a59650
Disha0123456789 <163165900+Disha0123456789@users.noreply.github.com>
2024-04-04 01:38:59 +0530

    Update EducationalGuidance.jsx

--- src/components/Astrology/EducationalGuidance.jsx
+++ src/components/Astrology/EducationalGuidance.jsx
@@ -11,14 +11,14 @@ function EducationalGuidance() {
         <div className="Container-heading">
           <h1>Educational Guidance</h1>
         </div>
-        <div className="content">
-          <div className="content-left">
+        <div className="conten">
+          <div className="conten-left">
             <div className="img-eg"></div>
             <button onClick={() => navigate("/Done", { state: { head, img } })}>
               Book Now
             </button>
           </div>
-          <div className="content-right">
+          <div className="conten-right">
             <p>
               Educational guidance involves providing support and direction to
               individuals in their academic pursuits and personal development.
