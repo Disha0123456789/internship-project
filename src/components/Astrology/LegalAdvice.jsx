415a623e8ae77587c40c7700663d7a7916924361
Disha0123456789 <163165900+Disha0123456789@users.noreply.github.com>
2024-04-04 01:40:07 +0530

    Update LegalAdvice.jsx

--- src/components/Astrology/LegalAdvice.jsx
+++ src/components/Astrology/LegalAdvice.jsx
@@ -11,14 +11,14 @@ function LegalAdvice() {
         <div className="Container-heading">
           <h1>Legal Advice</h1>
         </div>
-        <div className="content">
-          <div className="content-left">
+        <div className="conten">
+          <div className="conten-left">
             <div className="img-la"></div>
             <button onClick={() => navigate("/Done", { state: { head, img } })}>
               Book Now
             </button>
           </div>
-          <div className="content-right">
+          <div className="conten-right">
             <p>
               Legal advice refers to professional guidance provided by qualified
               attorneys or legal experts regarding legal rights,
