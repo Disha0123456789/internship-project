b6af6cd8f223863721bde2d0e1ca8f9a6958b94d
Disha0123456789 <163165900+Disha0123456789@users.noreply.github.com>
2024-04-04 15:00:15 +0530

    Update BirthChartInterpretation.jsx

--- src/components/Astrology/BirthChartInterpretation.jsx
+++ src/components/Astrology/BirthChartInterpretation.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./AstroMain.css";
 import { useNavigate } from "react-router-dom";
-
+import image from './images/Birth Chart Interpretation.png';
 function BirthChartInterpretation() {
   const navigate = useNavigate();
   const head = "Birth Chart Interpretation";
-  const img = "./images/Marriage Consultation.png";
+  const img = image;
   return (
     <div>
       <div className="Container">
