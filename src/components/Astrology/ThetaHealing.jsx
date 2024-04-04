c9ac86beb0158e42419d57f6aa020a2fe2c98463
Disha0123456789 <163165900+Disha0123456789@users.noreply.github.com>
2024-04-04 15:14:10 +0530

    Update ThetaHealing.jsx

--- src/components/Astrology/ThetaHealing.jsx
+++ src/components/Astrology/ThetaHealing.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./AstroMain.css";
 import { useNavigate } from "react-router-dom";
+import image from './images/Theta Healing.png';
 function ThetaHealing() {
   const navigate = useNavigate();
   const head = "Theta Healing";
-  const img = "./images/Marriage Consultation.png";
+  const img = image;
   return (
     <div>
       <div className="Container">
