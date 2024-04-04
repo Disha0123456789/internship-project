a1ae505285362ca86400a568182d5ca579ac68f5
Disha0123456789 <163165900+Disha0123456789@users.noreply.github.com>
2024-04-04 15:14:50 +0530

    Update WealthManagement.jsx

--- src/components/Astrology/WealthManagement.jsx
+++ src/components/Astrology/WealthManagement.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./AstroMain.css";
 import { useNavigate } from "react-router-dom";
+import image from './images/Wealth Management.png';
 function WealthManagement() {
   const navigate = useNavigate();
   const head = "Wealth Management";
-  const img = "./images/Marriage Consultation.png";
+  const img = image;
   return (
     <div>
       <div className="Container">
