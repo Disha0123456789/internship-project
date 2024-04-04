81f3040551dd14465afa335e817c05fc8128becc
Disha0123456789 <163165900+Disha0123456789@users.noreply.github.com>
2024-04-04 15:07:32 +0530

    Update LegalAdvice.jsx

--- src/components/Astrology/LegalAdvice.jsx
+++ src/components/Astrology/LegalAdvice.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./AstroMain.css";
 import { useNavigate } from "react-router-dom";
+import image from './images/Legal Advice.png';
 function LegalAdvice() {
   const navigate = useNavigate();
   const head = "Legal Advice";
-  const img = "./images/Marriage Consultation.png";
+  const img = image;
   return (
     <div>
       <div className="Container">
