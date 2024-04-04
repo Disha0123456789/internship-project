43dfb83d5049ecc354094c9fcc0b0287a9f0f56d
Disha0123456789 <163165900+Disha0123456789@users.noreply.github.com>
2024-04-04 15:05:57 +0530

    Update HealthHealing.jsx

--- src/components/Astrology/HealthHealing.jsx
+++ src/components/Astrology/HealthHealing.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./AstroMain.css";
 import { useNavigate } from "react-router-dom";
+import image from './images/Health Healing.png';
 function HealthHealing() {
   const navigate = useNavigate();
   const head = "Health Healing";
-  const img = "./images/Marriage Consultation.png";
+  const img = image;
   return (
     <div>
       <div className="Container">
