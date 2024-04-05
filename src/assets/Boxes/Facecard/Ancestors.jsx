d3615d2020d0898841d809bd86c2dc74f8425e4e
Disha0123456789 <163165900+Disha0123456789@users.noreply.github.com>
2024-04-06 01:22:13 +0530

    Update Ancestors.jsx

--- src/assets/Boxes/Facecard/Ancestors.jsx
+++ src/assets/Boxes/Facecard/Ancestors.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-
+import ancestorIcon from '../../../../public/2nd-row-reading/where your ancestors come from.png';
 function Ancestors() {
   return (
-    <div className="face-result-whole">
+    <div className="face-result">
       <h1 className="face-text-result">Know Your Past</h1>
       <div className="Know-past">
         <p className="personal-trait-text">Ancestral Roots</p>
         <img
           className="personal-trait-icon"
-          src="public\2nd row reading\where your ancestors come from.png"
+          src={ancestorIcon}
         ></img>
         <hr className="hr-tag"></hr>
       </div>
@@ -17,3 +17,4 @@ function Ancestors() {
 }
 
 export default Ancestors;
+
