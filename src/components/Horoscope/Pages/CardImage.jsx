a87fb7f5cf33d4996237b8acba1519f56cdfd6c8
Disha0123456789 <163165900+Disha0123456789@users.noreply.github.com>
2024-04-15 12:51:47 +0530

    Update CardImage.jsx

--- src/components/Horoscope/Pages/CardImage.jsx
+++ src/components/Horoscope/Pages/CardImage.jsx
@@ -7,7 +7,7 @@ export default function CardImage(props) {
       <div className='mycard'>
         <div className='img-ring'>
         <a href={myLink} style={{ display: 'flex', justifyContent: 'center' }}>
-            <img className='card-image' src={imagUrl} />
+            <img className='horo-card-image' src={imagUrl} />
           </a>
         </div>
         <span className='img-name'>{name} </span>
