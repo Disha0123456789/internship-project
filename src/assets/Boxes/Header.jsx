3e0f4961a39577e317dd7aa873f9f24b549f5ffc
Disha0123456789 <163165900+Disha0123456789@users.noreply.github.com>
2024-04-16 16:34:35 +0530

    Update Header.jsx

--- src/assets/Boxes/Header.jsx
+++ src/assets/Boxes/Header.jsx
@@ -41,7 +41,7 @@ function Header() {
               </InputGroup.Text>
             </InputGroup>
 
-            <Nav.Link className="me-3">
+            <Nav.Link onClick={() => navigate("/login_page")} className="me-3">
               Login/Registration
             </Nav.Link>
             <Nav.Link href="#link" className="me-3">
