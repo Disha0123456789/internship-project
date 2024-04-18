05cce8aa3c73d89e0ce1499698d880328077a930
Disha0123456789 <163165900+Disha0123456789@users.noreply.github.com>
2024-04-19 00:48:53 +0530

    Update TarotResult.jsx

--- src/Page/TarotReading/TarotResult.jsx
+++ src/Page/TarotReading/TarotResult.jsx
@@ -1,27 +1,25 @@
+// TarotResult.js
 import React from "react";
-import "./Cards.css";
 import { useLocation } from "react-router-dom";
-// import IMG from "./IMGS/red result background.jpg";
-import IMG from "./IMGS/result card.png";
+import IMG from "/IMGS/result card.png";
+
 const TarotResult = () => {
   const loc = useLocation();
-  console.log(loc.state.cardtext[loc.state.card][1]);
-  console.log("no");
+  
   return (
     <div className="Result-Tarot">
-      <div className="result-tarot-card">
+      <div className="tarot-result-card">
         <div className="tarot-head">
-          <h1>{loc.state.heads}</h1>
+          <h1>{loc.state.head}</h1>
         </div>
         <div className="tarot-card-image">
           <img src={IMG} alt="" />
-          <h3>{loc.state.cardhead[loc.state.card]}</h3>
+          <h3>{loc.state.title}</h3>
         </div>
         <div className="tarot-result-info">
           <div className="tarot-info">
-            <p>{loc.state.cardtext[loc.state.card][0]}</p>
-            <br />
-            <p>{loc.state.cardtext[loc.state.card][1]}</p>
+          <p>{loc.state.heading}</p>
+            <p>{loc.state.para}</p>
           </div>
         </div>
       </div>
