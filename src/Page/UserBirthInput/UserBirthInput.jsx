0d423b15d652a1232e73e9d1c6df825c6165c1b7
Disha0123456789 <163165900+Disha0123456789@users.noreply.github.com>
2024-04-21 23:16:59 +0530

    Update UserBirthInput.jsx

--- src/Page/UserBirthInput/UserBirthInput.jsx
+++ src/Page/UserBirthInput/UserBirthInput.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Select from 'react-select';
 //import code from './code.json';
-//import dummy from './dummy.json';
+import dummy from './dummy.json';
 import './UserBirthInput.css';
 import { useNavigate } from 'react-router-dom';
 
