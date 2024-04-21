a66e4fe274f3303bd7f26b684b66a45e8d86e7fd
khalid00103 <shaikhkhalid003@gmail.com>
2024-04-21 22:24:48 +0530

    future box

--- src/components/FutureBox/NewFutureBox.jsx
+++ src/components/FutureBox/NewFutureBox.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 const NewFutureBox = () => {
   const navigate = useNavigate();
+  const handleNumeroClick = () => {
+    navigate('/MainPage', { state: { nextPage: 'Numerology' } });
+  };
   return (
     <>
-      <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+      <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-12 mx-auto">
         <h1 className="text-3xl pb-10 font-semibold text-center">
           Read Your Future Now ! 
         </h1>
@@ -66,7 +69,7 @@ const NewFutureBox = () => {
         </div>
       </div>
 
-      <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+      <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-12 mx-auto">
         <h1 className="text-3xl pb-10 font-semibold text-center">
           Embark On Your Journey !
         </h1>
@@ -117,12 +120,14 @@ const NewFutureBox = () => {
 
       </div>
 
-      <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+      <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-12 mx-auto">
         <h1 className="text-3xl pb-10 font-semibold text-center">
           Lets Go To Another Dimension !
         </h1>
 
-          <div class="grid grid-cols-1 lg:grid-cols-3 gap-10 md:gap-49">
+        <div class="grid grid-cols-1 lg:grid-cols-5 gap-min-10px gap-max-20px">
+          <div class="hidden lg:block lg:col-span-1"></div>
+          
           <div 
             onClick={() => navigate("/dream")} 
             class="h-36 sm:h-50 w-60 flex flex-col justify-center border bg-gradient-to-tr from-purple-300 to-blue-300 rounded-xl text-center p-4 mx-auto md:p-5 dark:border-gray-700">
@@ -136,7 +141,7 @@ const NewFutureBox = () => {
               </h3>
             </div>
           </div>
-
+          {/* 
           <div class="h-36 sm:h-50 w-60 flex flex-col justify-center border bg-gradient-to-tr from-purple-300 to-blue-300 rounded-xl text-center p-4 mx-auto md:p-5 dark:border-gray-700">
             <div class="flex justify-center items-center size-20 rounded-lg mx-auto">
               <img src="/ICONS/crystalball.png" alt="19" />
@@ -148,8 +153,10 @@ const NewFutureBox = () => {
               </h3>
             </div>
           </div>
-
-          <div onClick={() => navigate("/Numerology")} 
+          */}
+          <div class="hidden lg:block lg:col-span-1"></div>
+          <div 
+          onClick={handleNumeroClick}
           class="h-36 sm:h-50 w-60 flex flex-col justify-center border bg-gradient-to-tr from-purple-300 to-blue-300 rounded-xl text-center p-4 mx-auto md:p-5 dark:border-gray-700">
               <div class="flex justify-center items-center size-20 rounded-lg mx-auto">
                 <img src="/ICONS/numerology.png" alt="16" />
@@ -165,12 +172,13 @@ const NewFutureBox = () => {
  
       </div>
 
-      <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+      <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-12 mx-auto">
         <h1 className="text-3xl pb-10 font-semibold text-center">
           Nourish Your Soul !
         </h1>
 
-        <div class="grid grid-cols-1 lg:grid-cols-3 gap-10 md:gap-15">
+        <div class="grid grid-cols-1 lg:grid-cols-5 gap-min-10px gap-max-20px">
+          <div class="hidden lg:block lg:col-span-1"></div>
           <div
             onClick={() => navigate("/Meditation")}
             class="h-36 sm:h-50 w-60 flex flex-col justify-center border bg-gradient-to-tr from-purple-300 to-blue-300 rounded-xl text-center p-4 mx-auto md:p-5 dark:border-gray-700">
@@ -185,17 +193,18 @@ const NewFutureBox = () => {
               </h3>
             </div>
           </div>
-
+          {/**  Divine Connection logo
           <div class="h-36 sm:h-50 flex flex-col justify-center">
             <img src="/ICONS/divinelogohorizontal.png" alt="14" />
-
+          */}
             {/* <div class="mt-3">
               <h3 class="text-md sm:text-lg font-semibold text-gray-800 dark:text-gray-200">
                 Meditation
               </h3>
-            </div> */}
+            </div> 
           </div>
-
+          */}
+          <div class="hidden lg:block lg:col-span-1"></div>
           <div
             onClick={() => navigate("/EmotionalSupport")}
             class="h-36 sm:h-50 w-60 flex flex-col justify-center border bg-gradient-to-tr from-purple-300 to-blue-300 rounded-xl text-center p-4 mx-auto md:p-5 dark:border-gray-700">
