bba18a5bd02a441dd0bdf0092731f18f1e958fe2
khalid00103 <shaikhkhalid003@gmail.com>
2024-07-20 02:24:18 +0530

    database integration

--- src/Page/Registration-Login/Register.jsx
+++ src/Page/Registration-Login/Register.jsx
@@ -1,92 +1,135 @@
-//task-2
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import logo from "/assets/Registration-Login/images/divine logo vertical.png";
 import gIcon from "/assets/Registration-Login/images/google-color-icon.png";
 import fIcon from "/assets/Registration-Login/images/facebook-app-round-icon.png";
 import "./Register.css";
-import { Link } from "react-router-dom"
-import { useNavigate } from "react-router-dom";  
 
 export default function Register() {
-      const navigate = useNavigate();
-    return (
-        <div className="Register-Page">
-            <div className='Register'>
-                <img src={logo} alt='logo' className='logo' />
-                <div className='register-title'> <h1>Register here</h1><br></br>
-                    <p>Create Your Account on Divine Connection</p>
-                </div>
-            </div>
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    phoneNo: '',
+    password: '',
+    confirmPassword: ''
+  });
+  const navigate = useNavigate();
 
-            <div class="form-container">
-                <form action="#">
-                    <div class="form-row">
-                        <div class="input-data">
-                            <input type="text" required />
-                            <div class="underline"></div>
-                            <label for="">First Name</label>
-                        </div>
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
 
-                        <div class="input-data">
-                            <input type="text" required />
-                            <div class="underline"></div>
-                            <label for="">Last Name</label>
-                        </div>
-                    </div>
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (formData.password !== formData.confirmPassword) {
+      alert("Passwords do not match");
+      return;
+    }
 
-                    <div class="form-row">
-                        <div class="input-data">
-                            <input type="text" required />
-                            <div class="underline"></div>
-                            <label for="">Email</label>
-                        </div>
+    try {
+      const response = await fetch('https://divineconnection.co.in:8005/api/auth/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          firstName: formData.firstName,
+          lastName: formData.lastName,
+          email: formData.email,
+          phoneNo: formData.phoneNo,
+          password: formData.password
+        })
+      });
+      if (response.ok) {
+        navigate('/login_page');
+      } else {
+        const result = await response.json();
+        alert(result.message);
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
 
-                        <div class="input-data">
-                            <input type="text" required />
-                            <div class="underline"></div>
-                            <label for="">Phone No.</label>
-                        </div>
-                    </div>
-
-                    <div class="form-row">
-                        <div class="input-data">
-                            <input type="text" required />
-                            <div class="underline"></div>
-                            <label for="">Password</label>
-                        </div>
-
-                        <div class="input-data">
-                            <input type="text" required />
-                            <div class="underline"></div>
-                            <label for="">Confirm Password</label>
-                        </div>
-                    </div>
-
-                    <div class="form-row-last">
-                        <input type="submit" id='register-submit' value="Register" />
-                        <p className='register-terms-message'>
-                            By signing up, you agree to our <Link to="/TermsAndConditions">Terms and Conditions</Link>, <Link to="/PrivacyPolicy">Privacy Policy</Link>.
-                        </p>
-                        <p className='register-Register-link'>Already have an account ? <Link to="/login_page">Login</Link></p>
-                        </div>
-                            <div className='login-text-two'>
-                                <hr />
-                                <p>or Signup With</p>
-                                <hr />
-                            </div>
-                            <div className='sign-up-icons'>
-                                <a href = "https://g.co/kgs/BCKMD8f">
-                                <img src={gIcon} alt="google" width={40} height={40} />
-                                </a>
-                                <a href='https://www.facebook.com/'>
-                                <img src={fIcon} alt="facebook" width={40} height={40} />
-                                </a>
-                            </div>
-
-
-                </form>
-            </div>
-
+  return (
+    <div className="Register-Page">
+      <div className='Register'>
+        <img src={logo} alt='logo' className='logo' />
+        <div className='register-title'> <h1>Register here</h1><br></br>
+          <p>Create Your Account on Divine Connection</p>
         </div>
-    )
+      </div>
+
+      <div className="form-container">
+        <form onSubmit={handleSubmit}>
+          <div className="form-row">
+            <div className="input-data">
+              <input type="text" name="firstName" value={formData.firstName} onChange={handleChange} required />
+              <div className="underline"></div>
+              <label>First Name</label>
+            </div>
+
+            <div className="input-data">
+              <input type="text" name="lastName" value={formData.lastName} onChange={handleChange} required />
+              <div className="underline"></div>
+              <label>Last Name</label>
+            </div>
+          </div>
+
+          <div className="form-row">
+            <div className="input-data">
+              <input type="text" name="email" value={formData.email} onChange={handleChange} required />
+              <div className="underline"></div>
+              <label>Email</label>
+            </div>
+
+            <div className="input-data">
+              <input type="text" name="phoneNo" value={formData.phoneNo} onChange={handleChange} required />
+              <div className="underline"></div>
+              <label>Phone No.</label>
+            </div>
+          </div>
+
+          <div className="form-row">
+            <div className="input-data">
+              <input type="password" name="password" value={formData.password} onChange={handleChange} required />
+              <div className="underline"></div>
+              <label>Password</label>
+            </div>
+
+            <div className="input-data">
+              <input type="password" name="confirmPassword" value={formData.confirmPassword} onChange={handleChange} required />
+              <div className="underline"></div>
+              <label>Confirm Password</label>
+            </div>
+          </div>
+
+          <div className="form-row-last">
+            <input type="submit" id='register-submit' value="Register" />
+            <p className='register-terms-message'>
+              By signing up, you agree to our <Link to="/TermsAndConditions">Terms and Conditions</Link>, <Link to="/PrivacyPolicy">Privacy Policy</Link>.
+            </p>
+            <p className='register-Register-link'>Already have an account ? <Link to="/login_page">Login</Link></p>
+          </div>
+          <div className='login-text-two'>
+            <hr />
+            <p>or Signup With</p>
+            <hr />
+          </div>
+          <div className='sign-up-icons'>
+            <a href="https://g.co/kgs/BCKMD8f">
+              <img src={gIcon} alt="google" width={40} height={40} />
+            </a>
+            <a href='https://www.facebook.com/'>
+              <img src={fIcon} alt="facebook" width={40} height={40} />
+            </a>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
 }
